6f69c15077b5696b599420a2f5d0bd67ba0d3cc3
Preetpal Sohal <preetpal.sohal@gmail.com>
2015-09-03 18:13:06 -0700

    Solved problem 4.

---
+++ 4.swift
@@ -0,0 +1,18 @@
+
+func isPalindrome(possiblePalindrome: String) -> Bool {
+    return possiblePalindrome == String(reverse(possiblePalindrome))
+}
+
+var bestSoFar = 0
+
+for x in 100..<1000 {
+    for y in x..<1000 {
+        let xy = x * y
+        if xy > bestSoFar && isPalindrome(String(xy)) {
+            bestSoFar = xy
+        }
+    }
+}
+
+// Solution: 906609
+println(bestSoFar)
