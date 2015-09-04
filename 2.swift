47fc92ce2409cf98fce5b958496961cb0ed7069b
Preetpal Sohal <preetpal.sohal@gmail.com>
2015-09-03 17:01:46 -0700

    Solved problem 2.

---
+++ 2.swift
@@ -0,0 +1,20 @@
+
+func nthFibonacciSequenceTerm(secondLastTerm: Int, lastTerm: Int) -> (lastTerm: Int, nthTerm: Int) {
+    return (lastTerm, secondLastTerm + lastTerm)
+}
+
+var fib1 = 1
+var fib2 = 2
+var total = fib2
+
+while (fib2 <= 4000000) {
+    let nextFibonacciSequenceTerms = nthFibonacciSequenceTerm(fib1, fib2)
+    fib1 = nextFibonacciSequenceTerms.lastTerm
+    fib2 = nextFibonacciSequenceTerms.nthTerm
+
+    if (fib2 % 2 == 0) {
+        total += fib2
+    }
+}
+
+println(total)
